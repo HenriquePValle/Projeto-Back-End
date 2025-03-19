6c75f9fe6d84638e86b20ef3e0257e55b31f27ea
Henrique de Padua Valle <henrique.valle@escola.pr.gov.br>
2025-03-19 10:56:33 -0300

    Update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,14 @@
-window.onload();
+function inicio(){
+    document.getElementById('menu').style.display = 'none';
+    document.querySelector("#botao-menu").addEventListener("click", abrirMenu);
+    document.querySelector("button.botao-pesquisa").addEventListener("click", abrirCaixaPesquisa);
+}
+
+function abrirMenu(){
+    let menu = document.getElementById("menu");
+    if(menu.style.display === 'none'){
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
